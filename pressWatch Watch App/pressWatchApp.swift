9631a20446e400e01fde06960beba4ed02c03d64
fedorez <fedorez@mail.ru>
2023-08-11 13:25:21 +0300

    some

---
+++ pressWatch Watch App/pressWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  pressWatchApp.swift
+//  pressWatch Watch App
+//
+//  Created by Denis Fedorets on 11.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct pressWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
